af75d91c126743d6cec66fdc89982257e0687edb
Brett Sira <brett@Bretts-MacBook-Pro.local>
2022-11-10 23:16:03 +1100

    add simple graphql simple use script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
-console.log('Script is executing.');
+import express from "express";
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from "graphql";
+
+/* GraphQL schema */
+const schema = buildSchema(`
+type Query {
+   root: String,
+   message1: String,
+   message2: String
+}
+`);
+
+/* Apply GraphQL schema */
+const root = () => ('root vallue');
+const message1 = () => ('message 1');
+const message2 = () => ('message 2');
+
+/* API configuration */
+const _CONF = {
+  port: 3030,
+};
+
+/* API setup */
+const api = express();
+api.use(express.json());
+
+/* Route */
+// root
+api.get('/api/root', 
+   graphqlHTTP({
+      schema: schema,      // select schema
+      rootValue: root,     // select root value
+      graphiql: true,
+   })
+);
+// message 1
+api.get('/api/message1', 
+   graphqlHTTP({
+      schema: schema,
+      rootValue: message1,
+      graphiql: true,
+   })
+);
+
+/* API listen and initialising function */
+api.listen(_CONF.port, () => {
+  console.log(`API is running on port ${_CONF.port}`);
+});
